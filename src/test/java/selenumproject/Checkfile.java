8c05781fb5f81dbd65aeb658a281ccabd4f077e3
richa688 <task.stadsolution@gmail.com>
2024-10-22 10:38:50 +0530

    third commit

---
+++ src/test/java/selenumproject/Checkfile.java
@@ -0,0 +1,11 @@
+package selenumproject;
+
+public class Checkfile {
+
+	public static void main(String[] args) {
+		
+		System.out.println("testing it");
+		System.out.println("this is check file");
+		}
+
+}
--- src/test/java/selenumproject/test.java
+++ src/test/java/selenumproject/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 	
 	System.out.println("Added new file");
+	System.out.println("this is new file");
 	}
 
 }
